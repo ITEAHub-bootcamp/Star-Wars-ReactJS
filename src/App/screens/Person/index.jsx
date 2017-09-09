d6d570c6c5515e89c0369086f7e40a2b0eb5c5a2
Olha Khomyak <khomyak.olha@gmail.com>
2017-09-09 08:52:32 +0300

    Add more info to person profile

--- src/App/screens/Person/index.jsx
+++ src/App/screens/Person/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Section from 'grommet/components/Section';
-import Heading from 'grommet/components/Heading';
+import Paragraph from 'grommet/components/Paragraph';
 import Hero from 'grommet/components/Hero';
 import Box from 'grommet/components/Box';
 import Image from 'grommet/components/Image';
 import List from 'grommet/components/List';
 import ListItem from 'grommet/components/ListItem';
+import Tiles from 'grommet/components/Tiles';
+import Tile from 'grommet/components/Tile';
+import Card from 'grommet/components/Card';
 
 import heroBg from './assets/star_wars_bg.jpg';
 
@@ -18,6 +21,7 @@ class Person extends React.PureComponent {
       userId: this.props.location.pathname.split('/')[2],
       userData: [],
       homePlanet: '',
+      filmsList: [],
     };
     this.apiUrl = 'https://swapi.co/api/people';
   }
@@ -29,6 +33,7 @@ class Person extends React.PureComponent {
       .then((json) => {
         this.setState({ userData: json });
         this.getPlanetName(json.homeworld);
+        this.getFilms(json.films);
       });
   }
   getPlanetName(planetUrl) {
@@ -36,6 +41,37 @@ class Person extends React.PureComponent {
       .fetch(planetUrl)
       .then(response => response.json())
       .then(json => this.setState({ homePlanet: json }));
+  }
+
+  getFilms(filmsUrl) {
+    filmsUrl.map((filmUrl, index) =>
+      window
+        .fetch(filmUrl)
+        .then(response => response.json())
+        .then(json => this.setState({ filmsList: this.state.filmsList.concat([json]) })),
+    );
+  }
+
+  get film() {
+    console.log(this.state.filmsList);
+    return this.state.filmsList && this.state.filmsList.length ?
+      this.state.filmsList.map(film =>
+        (<ListItem
+          key={film.url}
+          justify="between"
+          separator="horizontal"
+        >
+          <span className="secondary">
+            {film.title}, ({this.formatYear(film.release_date)})
+          </span>
+        </ListItem>
+        ))
+      :
+      false;
+  }
+  formatYear(date) {
+    let newDate = new Date(date);
+    return newDate.getFullYear();
   }
 
   render() {
@@ -51,39 +87,14 @@ class Person extends React.PureComponent {
           backgroundColorIndex="dark"
           size="large"
         >
-          <Box
-            direction="row"
-            justify="center"
-          >
-            <Box
-              basis="1/2"
-              align="end"
-              pad="medium"
-            />
-            <Box
-              basis="1/2"
-              align="start"
-              pad="medium"
-            >
-              <Box
-                pad="medium"
+          <Tiles>
+            <Tile>
+              <Card
+                heading={this.state.userData.name}
+                label={this.state.homePlanet.name}
                 colorIndex="grey-2-a"
               >
-                <Heading>
-                  {this.state.userData.name}
-                </Heading>
                 <List>
-                  <ListItem
-                    justify="between"
-                    separator="horizontal"
-                  >
-                    <span>
-                      Homeworld:
-                    </span>
-                    <span className="secondary">
-                      {this.state.homePlanet.name}
-                    </span>
-                  </ListItem>
                   <ListItem
                     justify="between"
                     separator="horizontal"
@@ -112,9 +123,19 @@ class Person extends React.PureComponent {
                     </span>
                   </ListItem>
                 </List>
-              </Box>
-            </Box>
-          </Box>
+              </Card>
+            </Tile>
+            <Tile>
+              <Card
+                label="Films"
+                colorIndex="grey-2-a"
+              >
+                <List>
+                  {this.film}
+                </List>
+              </Card>
+            </Tile>)
+          </Tiles>
         </Hero>
       </Section>
     );
